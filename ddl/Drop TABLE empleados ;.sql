966458e6b8e0e0150e21afefd9438cdca89cc8ca
CamiloLavado <camilolavado@petalmail.com>
2022-02-11 17:17:11 -0300

    solucion primera consulta parte final

--- ddl/Drop TABLE empleados ;.sql
+++ ddl/Drop TABLE empleados ;.sql
@@ -1,32 +1,32 @@
-Drop TABLE empleados ;
+SELECT * FROM empleados ;
 
 
-Drop TABLE clientes;
+SELECT * FROM clientes;
 
 
-Drop TABLE despachadores ;
+SELECT * FROM despachadores ;
 
 
-Drop TABLE categoria  ;
+SELECT * FROM categoria  ;
 
 
-Drop TABLE proveedores  ;
+SELECT * FROM proveedores  ;
 
 
-Drop TABLE productos ;
-
- 
-
-Drop TABLE compras ;
+SELECT * FROM productos ;
 
  
 
+SELECT * FROM compras ;
+
+ 
+
  
 
 
-ALTER TABLE productos ADD CONSTRAINT FK_productos__id_categoria FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria);
-ALTER TABLE productos ADD CONSTRAINT FK_productos__id_proveedor FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor);
-ALTER TABLE orden_de_compra ADD CONSTRAINT FK_orden_de_compra__id_producto FOREIGN KEY (id_producto) REFERENCES productos(id_producto);
-ALTER TABLE ordenes ADD CONSTRAINT FK_ordenes__id_compra FOREIGN KEY (id_compra) REFERENCES compras(id_compra);
-ALTER TABLE ordenes ADD CONSTRAINT FK_ordenes__id_empleado FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado);
-ALTER TABLE ordenes ADD CONSTRAINT FK_ordenes__id_despachador FOREIGN KEY (id_despachador) REFERENCES despachadores(id_despachador);
+ALTER * FROM productos ADD CONSTRAINT FK_productos__id_categoria FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria);
+ALTER * FROM productos ADD CONSTRAINT FK_productos__id_proveedor FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor);
+ALTER * FROM orden_de_compra ADD CONSTRAINT FK_orden_de_compra__id_producto FOREIGN KEY (id_producto) REFERENCES productos(id_producto);
+ALTER * FROM ordenes ADD CONSTRAINT FK_ordenes__id_compra FOREIGN KEY (id_compra) REFERENCES compras(id_compra);
+ALTER * FROM ordenes ADD CONSTRAINT FK_ordenes__id_empleado FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado);
+ALTER * FROM ordenes ADD CONSTRAINT FK_ordenes__id_despachador FOREIGN KEY (id_despachador) REFERENCES despachadores(id_despachador);
